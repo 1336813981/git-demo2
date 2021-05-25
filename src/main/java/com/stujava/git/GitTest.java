97db00ad0c987d2cb0e818cb3b8b9bb9bd2943db
Frank <lyf135983@163.com>
2021-05-25 18:41:28 +0800

    master commit2

---
+++ src/main/java/com/stujava/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.stujava.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git one");
+        System.out.println("hello git two");
+        System.out.println("hello git three");
+        System.out.println("hello git four");
+        System.out.println("master test commit");
+        System.out.println("hot-fix test");
+
+    }
+}
